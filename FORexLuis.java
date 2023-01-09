c16e5d819d34987777d79335ad55cf855c26ddeb
Miguel-hub22 <120652752+Miguel-hub22@users.noreply.github.com>
2023-01-09 14:16:44 -0300

    Update FORexLuis.java

--- FORexLuis.java
+++ FORexLuis.java
@@ -22,13 +22,58 @@ public class FORexLuis
 				n3 = leia.nextFloat();
 				
 				media = (n1+n2+n3)/3;
-				System.out.printf("\nM�dia: %3.2f",media);
+				System.out.printf("\nMédia: %3.2f",media);
 				somaMedia += media; // somaMedia = somaMedia + media
 			}
 			mediaGeral = somaMedia / 4;
-			System.out.println("\nM�dia Geral: "+mediaGeral);
+			System.out.println("\nMédia Geral: "+mediaGeral);
 
 		}
 
+	}
+}
+
+
+
+
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+
+package LacosDeRepeticao;
+
+
+	import java.util.Scanner;
+
+	public class Exercicio2ForeWhile {
+	/*Escreva um algoritmo em Java, que leia 10 números inteiros via teclado e mostre 
+	 *na tela quantos números são pares e quantos número são ímpares. Veja o exemplo abaixo:
+	 * 
+	 */
+
+	    // Exercicio 2 usando For
+	    public static void main(String[] args) {
+	        Scanner leia = new Scanner(System.in);
+
+	        int num,somaPar=0,somaImpar=0,x;
+
+
+	        for (x=1;x<=10;x++)
+	        {
+	            System.out.println("\nEntre com um número: ");
+	            num = leia.nextInt();
+	            if (num % 2 == 0)
+	            {
+	                somaPar++; // somaPar = somaPar + 1
+	            }
+	            else
+
+	                {
+	                    somaImpar++;
+	                }
+
+	            }
+	            System.out.printf("\nTemos: "+somaPar+" números pares...");
+	            System.out.printf ("\nTemos: "+somaImpar+" números ímpares...");
+	        }
 	}
 }
